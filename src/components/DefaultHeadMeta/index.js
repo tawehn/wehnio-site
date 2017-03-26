a7e11bc288afdb7c19ff29808d86bb87048012e4
Alex Wehn <alex@wehn.io>
2017-03-26 11:48:40 -0500

    Formatting changes

--- src/components/DefaultHeadMeta/index.js
+++ src/components/DefaultHeadMeta/index.js
@@ -4,28 +4,30 @@ import Helmet from "react-helmet"
 const DefaultHeadMeta = (props, { metadata: { pkg } }) => (
   <div hidden>
     <Helmet
-      meta={ [
+      meta={[
         {
           name: "generator", content: `${
-          process.env.PHENOMIC_NAME } ${ process.env.PHENOMIC_VERSION }`,
+          process.env.PHENOMIC_NAME} ${process.env.PHENOMIC_VERSION}`,
         },
         { property: "og:site_name", content: pkg.name },
-        { name: "twitter:site", content: `@${ pkg.twitter }` },
+        { name: "twitter:site", content: `@${pkg.twitter}` },
         ...props.meta ? props.meta : [],
-      ] }
-      script={ [
+      ]}
+      script={[
         { src: "https://cdn.polyfill.io/v2/polyfill.min.js" },
         ...props.scripts ? props.scripts : [],
-      ] }
+      ]}
     />
 
-    { /* meta viewport safari/chrome/edge */ }
+    { /* meta viewport safari/chrome/edge */}
     <Helmet
-      meta={ [ {
-        name: "viewport", content: "width=device-width, initial-scale=1",
-      } ] }
+      meta={[
+        {
+          name: "viewport", content: "width=device-width, initial-scale=1"},
+      { name: "robots", content: "noindex" }
+      ]}
     />
-    <style>{ "@-ms-viewport { width: device-width; }" }</style>
+    <style>{"@-ms-viewport { width: device-width; }"}</style>
   </div>
 )
 
