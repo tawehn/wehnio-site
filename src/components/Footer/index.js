5ec26902e9c9eab5447a12fa0c5d3c8b1038155a
Alex Wehn <alex@wehn.io>
2017-03-28 19:27:32 -0500

    Updated footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,16 +5,11 @@ import styles from "./index.css"
 const Footer = () => (
   <footer className={ styles.footer }>
     { /* If you like Phenomic, this is a way to share the love ;) */ }
-    <p>
-      <a
-        href={ process.env.PHENOMIC_HOMEPAGE }
-        className={ styles.phenomicReference }
-      >
-        { "Website generated with " }
-        <span className={ styles.phenomicReferenceName }>
-          {  `<${ process.env.PHENOMIC_NAME} />` }
-        </span>
-      </a>
+    <p className={styles.footerText}>
+    
+        Site generated with Phenomic / Hosted on Amazon S3 / <a href="/posts/about-this-site/">More Info</a>
+
+
     </p>
   </footer>
 )
