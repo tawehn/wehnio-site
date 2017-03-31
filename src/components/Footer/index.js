536dc36920c76e2124d3179cdd0e14bc3e5d0ce5
Alex Wehn <alex@wehn.io>
2017-03-31 16:14:12 -0500

    Fixed footer link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => (
     { /* If you like Phenomic, this is a way to share the love ;) */ }
     <p className={styles.footerText}>
     
-        Site generated with Phenomic / Hosted on Amazon S3 / <a href="/posts/about-this-site/">More Info</a>
+        Site generated with Phenomic / Hosted on Amazon S3 / <a href="/posts/building-wehn-io/">More Info</a>
 
 
     </p>
