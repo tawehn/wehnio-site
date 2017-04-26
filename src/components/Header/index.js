f7be7f8d88292ae21873ce72b02e342cc7171109
Alex Wehn <alex@wehn.io>
2017-04-25 21:23:32 -0500

    Made navbar transparent until scroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,40 @@
 import React, { PropTypes } from "react"
+import classNames from "classnames"
 import { Link } from "phenomic"
 import Svg from "react-svg-inline"
 
 import gitHubSvg from "../icons/iconmonstr-github-1.svg"
 import linkedinSvg from "../icons/iconmonstr-linkedin-2.svg"
-import emailSvg from "../icons/iconmonstr-email-1.svg"
 
 import styles from "./index.css"
 
-const Header = (props, { metadata: {
-        pkg
-    } }) => (
-        <header className={styles.header}>
+
+class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {scrolled: false};
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll.bind(this));
+    }
+
+    handleScroll(event) {
+        let scrollTop = event.srcElement.body.scrollTop;
+        if (scrollTop > 10) this.setState({scrolled: true});
+        else this.setState({scrolled: false});
+    }
+
+    render() {
+        let headerScrolledOptions = {};
+        headerScrolledOptions[styles.headerScrolled] = this.state.scrolled;
+        let classes = classNames(styles.header, headerScrolledOptions);
+        return <header className={classes}>
             <nav className={styles.nav}>
                 <div className={styles.navPart1}>
                     <Link className={styles.link} to={"/"} activeClassName={styles.activeLink}>
@@ -27,21 +50,24 @@ const Header = (props, { metadata: {
                 </div>
 
                 <div className={styles.navPart2}>
-                    {pkg.repository && <a href={pkg.repository} className={styles.link} target="github">
+                    {this.context.metadata.pkg.repository && <a href={this.context.metadata.pkg.repository} className={styles.link} target="github">
                         <Svg svg={gitHubSvg} cleanup /> <span className={styles.NavPart2ItemText}>{"GitHub"}</span>
                     </a>
                     }
-                    {pkg.linkedin && <a href={pkg.linkedin} className={styles.link} target="linkedin">
+                    {this.context.metadata.pkg.linkedin && <a href={this.context.metadata.linkedin} className={styles.link} target="linkedin">
                         <Svg svg={linkedinSvg} cleanup /> <span className={styles.NavPart2ItemText}>{"LinkedIn"}</span>
                     </a>
                     }
                 </div>
             </nav>
         </header>
-    )
+    }
+}
 
 Header.contextTypes = {
     metadata: PropTypes.object.isRequired
 }
+
+
 
 export default Header
